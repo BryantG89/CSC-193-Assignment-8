6870206e8cd7dc218e6999bade41d8dc4dbfa641
BryantG <Bgalo89@gmail.com>
2023-12-15 21:43:14 -0800

    Added endpoint to calculate circle area and circumference

--- app.js
+++ app.js
@@ -8,12 +8,37 @@ const app = express();
 app.use(express.static('public'));
 
 
-// First Endpoint to hello.js
-app.get('/hello', function (req, res) {
-    res.type('text');
-    res.send('Hello World!');
-  });
+// first Endpoint to hello.js
+app.get('/hello', function (req, res) 
+{
+  res.type('text');
+  res.send('Hello World!');
+});
   
+
+// endpoint to calculate circle area and circumference
+app.get('/math/circle/:r', function(req, res) 
+{
+  // get radius from URL parameter
+  const radius = parseFloat(req.params.r);
+
+  // Check if the radius is a valid number
+  if (isNaN(radius) || radius <= 0) 
+  {
+    // If the radius is not valid, send a 400 Bad Request response
+    res.status(400).json({ error: 'Invalid radius provided' });
+} else 
+  {
+    // Calculate the area and circumference
+    const area = Math.PI * radius * radius;
+    const circumference = Math.PI * 2 * radius;
+
+    // Send a JSON response with the calculated values
+    res.json({ area: area, circumference: circumference });
+
+  }
+});
+
 
 app.use(express.static('public'));
 const PORT = process.env.PORT || 8000;
