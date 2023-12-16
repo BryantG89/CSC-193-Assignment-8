3e071c22b3ab3c03155698518ff7d1c456c8b2c7
BryantG <Bgalo89@gmail.com>
2023-12-15 22:09:08 -0800

    Updated app.js code structure and layout

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
 'use strict';
+
 const express = require('express');
 const app = express();
 
-
-// define all endpoints here
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-
-// first Endpoint to hello.js
-app.get('/hello', function (req, res) 
-{
-  res.type('text');
-  res.send('Hello World!');
-});
-  
-
-// endpoint excercise #1: to calculate circle area and circumference
-app.get('/math/circle/:r', function(req, res) 
-{
-  // get radius from URL parameter
-  const radius = req.params.r;
-  
-  // Calculate the area and circumference
-  const area = Math.PI * radius * radius;
-  const circumference = Math.PI * 2 * radius;
-
-
-  // Check if the radius is valid 
-  if (isNaN(radius) || radius <= 0) 
-  {
-    // If the radius is not valid, send a 400 Bad Request response
-    res.status(400).json({ error: 'Invalid radius provided' });
-  } 
-  else 
-  {
-    // Send a JSON response with the calculated values
-    res.json({ area: area, circumference: circumference });
-    // res.type("text");
-    // res.send(`Area: ${area}, Circumference: ${circumference}`);
-  }
+// First Endpoint to hello.js
+app.get('/hello', function (req, res) {
+    res.type('text');
+    res.send('Hello World!');
 });
 
+// Endpoint to calculate circle area and circumference
+app.get('/math/circle/:r', function (req, res) {
+    // get radius from URL parameter
+    const radius = parseFloat(req.params.r);
 
-// app.use(express.static('public'));
+    // Check if the radius is a valid number
+    if (isNaN(radius) || radius <= 0) {
+        // If the radius is not valid, send a 400 Bad Request response
+        res.status(400).json({ error: 'Invalid radius provided' });
+    } else {
+        // Calculate the area and circumference
+        const area = Math.PI * radius * radius;
+        const circumference = Math.PI * 2 * radius;
+
+        // Send a JSON response with the calculated values
+        res.json({ area: area, circumference: circumference });
+    }
+});
+
 const PORT = process.env.PORT || 8000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 module.exports = app;
