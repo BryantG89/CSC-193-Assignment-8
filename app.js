6003fc0af22ae28bcbffcce4575d63cd4a06c4f8
BryantG <Bgalo89@gmail.com>
2023-12-15 21:06:52 -0800

    updated express and no demon

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const app = express();
 
 // define all endpoints here
 
-
 app.use(express.static('public'));
-const PORT = process.env.PORT || 8000;
-app.listen(PORT);
 
 
+// First Endpoint to hello.js
 app.get('/hello', function (req, res) {
     res.type('text');
     res.send('Hello World!');
   });
   
+
+app.use(express.static('public'));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
